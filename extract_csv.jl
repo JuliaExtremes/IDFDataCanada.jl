c9028d065d6849643d3b148cdd49c2d00316717f
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2019-06-25 12:20:56 -0400

    fix output format to CSV

--- extract_csv.jl
+++ extract_csv.jl
@@ -6,4 +6,4 @@ province = ["AB", "BC", "MB", "NB", "NL", "NS", "NT", "NU", "ON", "PE", "QC", "S
 url = "ftp://client_climate@ftp.tor.ec.gc.ca/Pub/Engineering_Climate_Dataset/IDF/idf_v3-00_2019_02_27/IDF_Files_Fichiers/"
 file_basename = "IDF_v3.00_2019_02_27"
 
-data_download(province, output_dir, url, file_basename)
+data_download(province, output_dir, url, file_basename, "CSV")
