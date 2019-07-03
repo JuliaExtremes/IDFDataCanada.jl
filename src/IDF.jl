e63d49b5ab9525b0663f51ed6666ece42bfd700a
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2019-07-03 10:48:31 -0400

    fix missing end

--- src/IDF.jl
+++ src/IDF.jl
@@ -5,5 +5,7 @@ using ClimateTools, CSV, DataFrames, Dates, Glob, NCDatasets
 include("functions.jl")
 include("mapping.jl")
 
-export get_idf, txt2csv, txt2netcdf, data_download, netcdf_generator,
+export get_idf, txt2csv, txt2netcdf, data_download, netcdf_generator
 export plotstation, plotstation_data
+
+end
