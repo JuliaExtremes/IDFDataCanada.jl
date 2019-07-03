347fdfaff7156ac7933bbb2b7c98f3604678cce0
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2019-07-03 15:44:12 -0400

    add import guard

--- src/mapping.jl
+++ src/mapping.jl
@@ -27,86 +27,88 @@ function plotstation_data(W::WeatherNetwork{<:Any}, data; reg="canada", N=12, ti
       return true, fig, ax, cbar
 end
 
-data_dir = "/Users/houton199/Documents/Stage_2019/data/QC"
-files = glob("*.nc", data_dir)
+if PROGRAM_FILE == @__FILE__
+      data_dir = "/Users/houton199/Documents/Stage_2019/data/QC"
+      files = glob("*.nc", data_dir)
 
-vari = "max_rainfall_amount_24h"
-stripchar = (s, r) -> replace(s, Regex("[$r]") => " ")
-title = "$(stripchar(vari, "_"))"
+      vari = "max_rainfall_amount_24h"
+      stripchar = (s, r) -> replace(s, Regex("[$r]") => " ")
+      title = "$(stripchar(vari, "_"))"
 
-A = loadstation(files, vari)
-lon, lat = ClimateTools.getnetworkcoords(A)
+      A = loadstation(files, vari)
+      lon, lat = ClimateTools.getnetworkcoords(A)
 
-#-----------------------------------------------------------------------------------------
-# Map of QC stations
-status, fig, ax, m1 = mapclimgrid(region="quebec")
-x, y = m1(lon, lat)
-cs = m1.scatter(x, y)
-ClimateTools.title("QC Weather stations")
-ClimateTools.PyPlot.savefig("/Users/houton199/Documents/Stage_2019/maps/stations_qc.png", dpi=300)
+      #-----------------------------------------------------------------------------------------
+      # Map of QC stations
+      status, fig, ax, m1 = mapclimgrid(region="quebec")
+      x, y = m1(lon, lat)
+      cs = m1.scatter(x, y)
+      ClimateTools.title("QC Weather stations")
+      ClimateTools.PyPlot.savefig("/Users/houton199/Documents/Stage_2019/maps/stations_qc.png", dpi=300)
 
-#-----------------------------------------------------------------------------------------
-# Mean
-data = []
-for i=1:length(A)
-      mm = mean(A[i])
-      push!(data, mm)
+      #-----------------------------------------------------------------------------------------
+      # Mean
+      data = []
+      for i=1:length(A)
+            mm = mean(A[i])
+            push!(data, mm)
+      end
+      titlestr = "Mean $(title) QC"
+      filename = "/Users/houton199/Documents/Stage_2019/maps/mean_$(vari)_qc.png"
+      plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
+
+      #-----------------------------------------------------------------------------------------
+      # Maximum
+      data = []
+      for i=1:length(A)
+            mm = maximum(A[i])
+            push!(data, mm)
+      end
+      titlestr = "Maximum $(title) QC"
+      filename = "/Users/houton199/Documents/Stage_2019/maps/max_$(vari)_qc.png"
+      plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
+
+      #-----------------------------------------------------------------------------------------
+      # Minimum
+      data = []
+      for i=1:length(A)
+            mm = minimum(A[i])
+            push!(data, mm)
+      end
+      titlestr = "Minimum $(title) QC"
+      filename = "/Users/houton199/Documents/Stage_2019/maps/min_$(vari)_qc.png"
+      plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
+
+      #-----------------------------------------------------------------------------------------
+      # Std dev
+      data = []
+      for i=1:length(A)
+            mm = std(A[i])
+            push!(data, mm)
+      end
+      titlestr = "Standard deviation $(title) QC"
+      filename = "/Users/houton199/Documents/Stage_2019/maps/std_$(vari)_qc.png"
+      plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
+
+      #-----------------------------------------------------------------------------------------
+      # Variance
+      data = []
+      for i=1:length(A)
+            mm = var(A[i])
+            push!(data, mm)
+      end
+      titlestr = "Variance $(title) QC"
+      filename = "/Users/houton199/Documents/Stage_2019/maps/var_$(vari)_qc.png"
+      plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
+
+      #-----------------------------------------------------------------------------------------
+      # Number of obs
+      data = []
+      for i=1:length(A)
+            mm = length(A[i])
+            push!(data, mm)
+      end
+      titlestr = "Number of observation per station\n $(title) QC"
+      filename = "/Users/houton199/Documents/Stage_2019/maps/nbobs_$(vari)_qc.png"
+      plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
 end
-titlestr = "Mean $(title) QC"
-filename = "/Users/houton199/Documents/Stage_2019/maps/mean_$(vari)_qc.png"
-plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
-
-#-----------------------------------------------------------------------------------------
-# Maximum
-data = []
-for i=1:length(A)
-      mm = maximum(A[i])
-      push!(data, mm)
-end
-titlestr = "Maximum $(title) QC"
-filename = "/Users/houton199/Documents/Stage_2019/maps/max_$(vari)_qc.png"
-plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
-
-#-----------------------------------------------------------------------------------------
-# Minimum
-data = []
-for i=1:length(A)
-      mm = minimum(A[i])
-      push!(data, mm)
-end
-titlestr = "Minimum $(title) QC"
-filename = "/Users/houton199/Documents/Stage_2019/maps/min_$(vari)_qc.png"
-plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
-
-#-----------------------------------------------------------------------------------------
-# Std dev
-data = []
-for i=1:length(A)
-      mm = std(A[i])
-      push!(data, mm)
-end
-titlestr = "Standard deviation $(title) QC"
-filename = "/Users/houton199/Documents/Stage_2019/maps/std_$(vari)_qc.png"
-plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
-
-#-----------------------------------------------------------------------------------------
-# Variance
-data = []
-for i=1:length(A)
-      mm = var(A[i])
-      push!(data, mm)
-end
-titlestr = "Variance $(title) QC"
-filename = "/Users/houton199/Documents/Stage_2019/maps/var_$(vari)_qc.png"
-plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
-
-#-----------------------------------------------------------------------------------------
-# Number of obs
-data = []
-for i=1:length(A)
-      mm = length(A[i])
-      push!(data, mm)
-end
-titlestr = "Number of observation per station\n $(title) QC"
-filename = "/Users/houton199/Documents/Stage_2019/maps/nbobs_$(vari)_qc.png"
-plotstation_data(A, data, titlestr=titlestr, filename=filename, N=8, reg="quebec")
