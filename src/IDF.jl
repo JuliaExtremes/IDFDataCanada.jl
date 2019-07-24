dfc557d44e6a3c367567008803cae81fd5ea885a
Gabriel Gobeil <gabriel.gobeil199@gmail.com>
2019-07-24 11:56:25 -0400

    fix exported functions

--- src/IDF.jl
+++ src/IDF.jl
@@ -3,9 +3,11 @@ module IDF
 using ClimateTools, CSV, DataFrames, Dates, Glob, NCDatasets
 
 include("functions.jl")
-include("mapping.jl")
+#include("mapping.jl")
 
 export get_idf, txt2csv, txt2netcdf, data_download, netcdf_generator
-export plotstation, plotstation_data
+export plotweatherstation, plotweatherstation_data
+export network_calculator
+#export mapweathernetwork
 
 end
