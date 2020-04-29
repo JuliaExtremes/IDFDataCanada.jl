20f206cffb25ff55aec533048fb1c06c4c7036a2
Gabriel <gabriel.gobeil199@gmail.com>
2020-04-29 14:07:30 -0400

    remove conflicting libraries

--- src/IDF.jl
+++ src/IDF.jl
@@ -1,13 +1,14 @@
 module IDF
 
-using ClimateTools, CSV, DataFrames, Dates, Glob, NCDatasets
+using CSV, DataFrames, Dates, Glob, NCDatasets
+#using ClimateTools
 
 include("functions.jl")
 #include("mapping.jl")
 
 export get_idf, txt2csv, txt2netcdf, data_download, netcdf_generator
-export plotweatherstation, plotweatherstation_data
-export network_calculator
+#export plotweatherstation, plotweatherstation_data
+#export network_calculator
 #export mapweathernetwork
 
 end
