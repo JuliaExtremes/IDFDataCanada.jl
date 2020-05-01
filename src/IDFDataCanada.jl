1315c1318108b2c65417ed02cc7dfbc52323bbbd
Gabriel <gabriel.gobeil199@gmail.com>
2020-05-01 12:59:51 -0400

    remove unused functions

--- src/IDFDataCanada.jl
+++ src/IDFDataCanada.jl
@@ -1,14 +1,9 @@
 module IDFDataCanada
 
 using AxisArrays, CSV, DataFrames, Dates, Glob, HTTP, LibCURL, NCDatasets
-#using ClimateTools
 
 include("functions.jl")
-#include("mapping.jl")
 
 export get_idf, txt2csv, txt2netcdf, data_download, netcdf_generator, drive_download
-#export plotweatherstation, plotweatherstation_data
-#export network_calculator
-#export mapweathernetwork
 
-end
+end 
